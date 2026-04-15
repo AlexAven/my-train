36edca4f550bc3a9e13aa095ba4ea882a3bc27ed
Alexander Kazakov <gloom31@gmail.com>
2026-04-15 13:23:46 +0300

    minor changes

---
+++ src/app/components/navigation/navigation.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link';
+
+import styles from './navigation.module.css';
+
+const Navigation = () => {
+  return (
+    <nav className={styles.navigation}>
+      <Link href="/" className={styles.navLink}>
+        Главная
+      </Link>
+      <Link href="/users" className={styles.navLink}>
+        Пользователи
+      </Link>
+    </nav>
+  );
+};
+
+export default Navigation;
